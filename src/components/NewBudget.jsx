f093e75858ed82e7b8f6dcc9fb41e91fa0c441bc
dqarias <dino.quispea@gmail.com>
2023-04-14 11:13:11 -0500

    Add validations for invalid inputs

--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
+import Message from './Message'
 
 const NewBudget = ({budget, setBudget}) => {
+
+    const [message, setMessage] = useState('')
     const handleOnChange = (e) => {
         setBudget(e.target.value)
     }
+
+    const handleBudget = (e) => {
+        e.preventDefault()
+        if (!Number(budget) || Number(budget) < 0){
+            setMessage('is an invalid budget')
+        }else{
+            console.log('valid')
+        }
+    }
+
   return (
     <div className='container-budget container shadow'>
-        <form className='form'>
+        <form onSubmit={handleBudget} className='form'>
             <div className='field'>
                 <label htmlFor="">Define your budget</label>
                 <input
@@ -16,6 +30,7 @@ const NewBudget = ({budget, setBudget}) => {
                 value={budget}
                 onChange={handleOnChange} />
                 <input type="submit" value='Add'/>
+                {message && <Message type="error">{message}</Message>}
             </div>
 
         </form>
